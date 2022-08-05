37190b2a546cfadb282f23db92491b9b17202abe
Светлана Львова <73392762+SvetlanaLvova@users.noreply.github.com>
2022-08-05 17:18:39 +0300

    Done

--- src/custom/useLocalStorage.js
+++ src/custom/useLocalStorage.js
@@ -1,13 +1,10 @@
 // КАСТОМНЫЙ ХУК
+
 import { useEffect, useState } from "react"
 
 function useLocalStorage(initState, localStorageKey) { //обертка над существующими хуками
-// initState значение по умолчанию
-// localStorageKey ключ для хранения данных
 
 const dataFromLS = JSON.parse(window.localStorage.getItem(localStorageKey)) 
-// достает данные из localStorage, когда запускается функция useLocalStorage
-//JSON.parse так как localStorage хранит только строки
 
 const data = dataFromLS ? dataFromLS : initState
 
